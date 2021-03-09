01c23af48fd0593dac1e445d03ef40087b5476a1
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-03-09 12:36:26 +0200

    Add evenOrOdd.js

---
+++ JS-Programming-Basics/Homeworks-And-Labs/Conditional-Statements-Lab/03.EvenOrOdd/evenOrOdd.js
@@ -0,0 +1,10 @@
+function evenOrOdd(numInput) {
+    let num = Number(numInput);
+
+    if (num % 2 === 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+}
+evenOrOdd("2");
