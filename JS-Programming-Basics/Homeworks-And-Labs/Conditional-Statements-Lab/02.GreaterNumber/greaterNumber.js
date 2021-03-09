c4f1ca2fb4419e1766fb70667a92fb216fab8220
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-03-09 12:34:19 +0200

    Add greaterNumber.js

---
+++ JS-Programming-Basics/Homeworks-And-Labs/Conditional-Statements-Lab/02.GreaterNumber/greaterNumber.js
@@ -0,0 +1,13 @@
+function greaterNumber(num1, num2) {
+    let number1 = Number(num1);
+    let number2 = Number(num2);
+
+    if (number1 > number2) {
+        console.log(number1);
+
+    } else {
+        console.log(number2);
+    }
+}
+
+greaterNumber("5", "3");
