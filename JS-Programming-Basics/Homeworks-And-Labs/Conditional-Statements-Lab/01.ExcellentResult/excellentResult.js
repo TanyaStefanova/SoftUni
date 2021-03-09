f90fb81083c76a2653403d74abab9731cfedef4a
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-03-09 12:31:19 +0200

    Add excellentResult.js

---
+++ JS-Programming-Basics/Homeworks-And-Labs/Conditional-Statements-Lab/01.ExcellentResult/excellentResult.js
@@ -0,0 +1,11 @@
+function grades(arg1) {
+    let grade = Number(arg1);
+    if (grade >= 5.50) {
+        console.log("Excellent" + "!");
+
+    } else {
+        console.log("Not Exellent!");
+        console.log("test");
+    }
+}
+grades("5.50");
