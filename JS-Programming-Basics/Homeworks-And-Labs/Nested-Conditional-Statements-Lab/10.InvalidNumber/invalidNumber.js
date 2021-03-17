23356471f5b58f7788c04f8f105f1a1c4cb14ccf
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-03-17 11:44:51 +0200

    Add invalidNumber.js

---
+++ JS-Programming-Basics/Homeworks-And-Labs/Nested-Conditional-Statements-Lab/10.InvalidNumber/invalidNumber.js
@@ -0,0 +1,9 @@
+function invalidNumber(arg){
+let number = Number(arg);
+let isValid = number >= 100 && number <= 200 || number === 0;
+if(!isValid){
+    console.log("invalid");
+
+}
+}
+invalidNumber("100")
