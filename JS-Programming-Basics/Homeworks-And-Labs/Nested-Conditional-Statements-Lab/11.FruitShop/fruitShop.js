ca8c36d34d90d21c0555a789de5dc60b5907278d
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-03-18 11:02:43 +0200

    Add fruitShop.js

---
+++ JS-Programming-Basics/Homeworks-And-Labs/Nested-Conditional-Statements-Lab/11.FruitShop/fruitShop.js
@@ -0,0 +1,65 @@
+function fruitShop(arg1, arg2, arg3) {
+    let fruit = arg1;
+    let day = arg2;
+    let quantity = Number(arg3);
+
+    if (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday') {
+      switch (fruit) {
+          case 'banana':
+              console.log((quantity * 2.50).toFixed(2));
+              break;
+          case 'apple':
+              console.log((quantity * 1.20).toFixed(2));
+              break;
+          case 'orange':
+              console.log((quantity * 0.85).toFixed(2));
+              break;
+          case 'grapefruit':
+              console.log((quantity * 1.45).toFixed(2));
+              break;
+          case 'kiwi':
+              console.log((quantity * 2.70).toFixed(2));
+              break;
+          case 'pineapple':
+              console.log((quantity * 5.50).toFixed(2));
+              break;
+          case 'grapes':
+              console.log((quantity * 3.85).toFixed(2));
+              break;
+          default:
+              console.log('error');
+              break;
+      }
+  } else if (day === 'Saturday' || day === 'Sunday') {
+      switch (fruit) {
+          case 'banana':
+              console.log((quantity * 2.70).toFixed(2));
+              break;
+          case 'apple':
+              console.log((quantity * 1.25).toFixed(2));
+              break;
+          case 'orange':
+              console.log((quantity * 0.90).toFixed(2));
+              break;
+          case 'grapefruit':
+              console.log((quantity * 1.60).toFixed(2));
+              break;
+          case 'kiwi':
+              console.log((quantity * 3.00).toFixed(2));
+              break;
+          case 'pineapple':
+              console.log((quantity * 5.60).toFixed(2));
+              break;
+          case 'grapes':
+              console.log((quantity * 4.20).toFixed(2));
+              break;
+          default:
+              console.log('error');
+              break;
+      }
+    } else {
+        console.log('error');
+    }
+
+}
+fruitShop("banana", "Monday", "2")
