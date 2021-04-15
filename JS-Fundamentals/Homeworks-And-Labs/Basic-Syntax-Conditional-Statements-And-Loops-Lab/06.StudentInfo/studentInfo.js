2b0eed8cf1a4c4daa9058d4123bba51db4878e7f
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-04-15 10:22:45 +0300

    Add studentInfo.js, monthPrinter.js and foreignLanguages.js

---
+++ JS-Fundamentals/Homeworks-And-Labs/Basic-Syntax-Conditional-Statements-And-Loops-Lab/06.StudentInfo/studentInfo.js
@@ -0,0 +1,8 @@
+function studentInfo(arg1, arg2, arg3){
+let name = arg1;
+let age = arg2;
+let grade = arg3;
+
+	console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+studentInfo('John', 15, 5.54678)
---
+++ JS-Fundamentals/Homeworks-And-Labs/Basic-Syntax-Conditional-Statements-And-Loops-Lab/08.ForeignLanguages/foreignLanguages.js
@@ -0,0 +1,12 @@
+function foreignLanguage(arg) {
+    let country = arg;
+
+    if (country === "England" || country === "USA") {
+        console.log("English");
+    } else if (country === "Spain" || country === "Argentina" || country === "Mexico") {
+        console.log("Spanish");
+    } else {
+        console.log("unknown");
+    }
+}
+foreignLanguage("Germany")
