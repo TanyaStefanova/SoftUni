9720844f039dea9be7aa34ecf9b5d03de1d643ac
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-04-20 10:29:59 +0300

    Add sumDigits.js, charsToString.js and townInfo.js

---
+++ JS-Fundamentals/Homeworks-And-Labs/Data-Types-And-Variables-Exercise/01.SumDigits/sumDigits.js
@@ -0,0 +1,10 @@
+function sumDigits(num) {
+    let sum = 0;
+    num = num.toString();
+
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+    console.log(sum);
+}
+sumDigits(543);
