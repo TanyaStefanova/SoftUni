b44ff639bf063dd812f026309f1e1f334294a753
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-04-26 10:42:50 +0300

    Add maxSequence.js and magicSum.js

---
+++ JS-Fundamentals/Homeworks-And-Labs/Arrays-Exercise/08.MagicSum/magicSum.js
@@ -0,0 +1,12 @@
+function magicSum(arr, n) {
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            let sum = arr[i] + arr[j]
+            if ( sum === n) {                
+                console.log(`${arr[i]} ${arr[j]}`);
+            }
+        }
+    }
+}
+magicSum([6, 5, 3, 4, 3, 3], 7)
