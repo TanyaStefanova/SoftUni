5e3844917423c8647155580d7d54263aca106268
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2021-04-29 10:50:20 +0300

    Add sortNumbers.js and englishNameOfLastDigit.js

---
+++ JS-Fundamentals/Homeworks-And-Labs/Basic-Syntax-Conditional-Statements-And-Loops-Exercise/More-Exercise/02.EnglishNameOfLastDigit/englishNameOfLastDigit.js
@@ -0,0 +1,49 @@
+function englishNameLastDigit(num) {
+    // let lastDigit = num % 10;
+
+    // if (lastDigit < 0) {
+    //     lastDigit = Math.abs(lastDigit);
+    // }
+
+    let input = num.toString().split('');
+    let lastDigit = Number(input[input.length - 1]);
+
+    let englishName = '';
+
+    switch (lastDigit) {
+        case 0:
+            englishName = 'zero';
+            break;
+        case 1:
+            englishName = 'one';
+            break;
+        case 2:
+            englishName = 'two';
+            break;
+        case 3:
+            englishName = 'three';
+            break;
+
+        case 4:
+            englishName = 'four';
+            break;
+        case 5:
+            englishName = 'five';
+            break;
+        case 6:
+            englishName = 'six';
+            break;
+        case 7:
+            englishName = 'seven';
+            break;
+        case 8:
+            englishName = 'eight';
+            break;
+        case 9:
+            englishName = 'nine';
+            break;
+    }
+
+    console.log(englishName);
+}
+englishNameLastDigit(512)
