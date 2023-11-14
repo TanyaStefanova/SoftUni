0ab211405e3678a60afeb7e02f19b0731dcdfb91
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2023-11-14 22:23:08 +0200

    Added first draft of React project - created Topbar, Navigation and Hero components

---
+++ JS-Advanced/ReactJS-Project/src/App.jsx
@@ -0,0 +1,16 @@
+import Topbar from './Topbar';
+import Navigation from './Navigation';
+import Hero from './Hero';
+import './css/style.css'
+function App() {
+
+  return (
+    <>
+    <Topbar />
+    <Navigation />
+    <Hero />
+     </>
+  )
+}
+
+export default App;
